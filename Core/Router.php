339fabbee41b877c90d7065d05976c51054601e9
Patryk Pilek <patryk.pilek@gmail.com>
2022-11-09 01:40:44 +0000

    Match the requested route to the list of routes in the routing table

--- Core/Router.php
+++ Core/Router.php
@@ -14,6 +14,12 @@ class Router
     protected $routes = [];
 
     /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $params = [];
+
+    /**
      * Add a route to the routing table
      *
      * @param string $route  The route URL
@@ -34,5 +40,35 @@ class Router
     public function getRoutes()
     {
         return $this->routes;
+    }
+
+    /**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     *
+     * @return boolean  true if a match found, false otherwise
+     */
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the currently matched parameters
+     *
+     * @return array
+     */
+    public function getParams()
+    {
+        return $this->params;
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
   - Configure the web server to have pretty URLs
   - Create amd require (not include) the router class
   - Create the routing table in the router, and add some routes
+  - Match the requested route to the list of routes in the routing table
